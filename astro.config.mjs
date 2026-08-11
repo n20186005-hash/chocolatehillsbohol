3f7b0a19f8b044dcd88db6f8945df1bef77179cb
n20186005-hash <n20186005@gmail.com>
2026-08-11 20:03:52 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 import { SITE } from './src/consts';
 
 export default defineConfig({
   site: SITE.url || undefined,
   integrations: [
+    tailwind(),
     ...(SITE.url ? [sitemap()] : []),
   ],
   output: 'server',
